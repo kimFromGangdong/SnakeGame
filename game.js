2fa73b345f7c34a661f427d5a8c8d656c680f5c8
kimFromGangdong <kkim360@naver.com>
2023-08-15 10:17:15 +0000

    Set a threshold with if statement to control frequency of animation frame

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
 let lastRenderTime = 0;
+let SNAKE_SPEED = 2;
 
 function main(currentTime){
-	//divide by 1000 to transfer from miliseconds to seconds
-	const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
-	lastRenderTime = currentTime;
 	window.requestAnimationFrame(main);
-	console.log(secondsSinceLastRender);
+	// divide by 1000 to transfer from miliseconds to seconds
+	// current time varies every 0.006second but 
+	// lastRenderTime changes when secondsSinceLastRender is bigger than 1/snake_speed
+	const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
+	if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+	console.log("render");
+	lastRenderTime = currentTime;
 }
 
 window.requestAnimationFrame(main);
