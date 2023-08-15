9bb339602930cd2bae880cd107f7557d2d3fba20
kimFromGangdong <kkim360@naver.com>
2023-08-15 07:58:27 +0000

    Found out that we have 0.016seconds of delay per every animation frame -descent for this game

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
+let lastRenderTime = 0;
+
 function main(currentTime){
-	console.log(currentTime);
+	//divide by 1000 to transfer from miliseconds to seconds
+	const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
+	lastRenderTime = currentTime;
 	window.requestAnimationFrame(main);
+	console.log(secondsSinceLastRender);
 }
+
 window.requestAnimationFrame(main);
