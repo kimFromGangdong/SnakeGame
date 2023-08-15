4af04c6503e1f0ffa7b2ec770e8a8f5080e1fbad
kimFromGangdong <kkim360@naver.com>
2023-08-15 10:32:20 +0000

    created snake.js file which will hold snake values and functions, imported them to game.js

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js';
+
 let lastRenderTime = 0;
-let SNAKE_SPEED = 2;
 
 function main(currentTime){
 	window.requestAnimationFrame(main);
+	
 	// divide by 1000 to transfer from miliseconds to seconds
 	// current time varies every 0.006second but 
 	// lastRenderTime changes when secondsSinceLastRender is bigger than 1/snake_speed
 	const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
 	if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-	console.log("render");
 	lastRenderTime = currentTime;
+	
+	//update function updates the conditions of a snake
+	update()
+	
+	//draw function renders the snake
+	draw()
 }
 
 window.requestAnimationFrame(main);
+
+function update(){
+	updateSnake();
+}
+
+function draw(){
+	drawSnake();
+}
---
+++ snake.js
@@ -0,0 +1,11 @@
+//snake.js contains all of the proporties of the snake
+
+export let SNAKE_SPEED = 2;
+
+export function update(){
+	console.log("updateSnake")
+}
+
+export function draw(){
+	console.log("drawSnake")
+}
