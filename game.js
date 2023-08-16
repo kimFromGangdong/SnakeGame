13f6486b21ac53a50aebdb13f02ad0c3b7c6f31f
kimFromGangdong <kkim360@naver.com>
2023-08-16 13:36:25 +0000

    Seperated snake body from the snake head plus, body follows head even if it changes it's direction

--- game.js
+++ game.js
@@ -27,5 +27,6 @@ function update(){
 }
 
 function draw(){
+	gameBoard.innerHTML = '';
 	drawSnake(gameBoard);
 }
--- snake.js
+++ snake.js
@@ -4,11 +4,17 @@ export let SNAKE_SPEED = 2;
 const snakeBody = [
 	{ x: 10, y: 11 },
 	{ x: 11, y: 11 },
-	{ x: 12, y: 11 }
+	{ x: 12, y: 11 },
+	{ x: 13, y: 11 },
+	{ x: 14, y: 11 }
 ];
 
 export function update(){
-	// console.log("updateSnake")
+	for(let i=snakeBody.length-2; i>=0; i--){
+		snakeBody[i+1] = {...snakeBody[i]};
+	}
+	snakeBody[0].x +=0;
+	snakeBody[0].y +=1;
 }
 
 export function draw(gameBoard){
